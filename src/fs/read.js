170aef0d8f7651ac0ef4e916409b892bb0b29c4c
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 15:33:39 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) throw Error('FS operation failed');
+    console.log(data);
+  });
 };
 
 await read();
