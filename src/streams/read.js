fe181bb2fb00a5abc4be09c2bd101204f46babdb
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 17:51:08 +0300

    feat: read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(filePath, "utf8");
+  stream.on("data", (data) => process.stdout.write(data));
+  stream.on("error", (err) => console.log(`Err: ${err}`));
 };
 
 await read();
