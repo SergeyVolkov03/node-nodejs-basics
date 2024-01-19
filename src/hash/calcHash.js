2562d2d72108a221d003a991cfebc1d5ad1c42ec
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 17:28:08 +0300

    feat: implement calc hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from "node:crypto";
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(filePath, "utf8");
+  stream.on("data", (data) => console.log(createHash("sha256").update(data).digest("hex")));
+  stream.on("error", (err) => console.log(`Err: ${err}`));
 };
 
 await calculateHash();
