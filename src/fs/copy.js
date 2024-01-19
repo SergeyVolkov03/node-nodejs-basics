0f4c73d2904fb5976be1852064a74174ee5f8bd4
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 14:45:10 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folderPath = path.join(__dirname, "/files");
+const copyFolderPath = path.join(__dirname, "/files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  if (!fs.existsSync(folderPath) || fs.existsSync(copyFolderPath)) {
+    throw Error("FS operation failed");
+  } else {
+    fs.mkdir(copyFolderPath, (err) => {
+      if (err) throw err;
+    });
+    fs.readdir(folderPath, "utf8", (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        fs.readFile(path.join(folderPath, file), "utf8", (err, data) => {
+          if (err) throw err;
+          fs.writeFile(path.join(copyFolderPath, file), data, "utf8", (err) => {
+            if (err) throw err;
+          });
+        });
+      });
+      console.log('Folder copied');
+    });
+  }
 };
 
 await copy();
