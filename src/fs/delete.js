1cb3f822a59c01c74a53ef249ddb666e86874847
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 15:25:19 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  fs.unlink(filePath, (err) => {
+    if (err) throw Error("FS operation failed");
+    console.log("File deleted");
+  });
 };
 
 await remove();
