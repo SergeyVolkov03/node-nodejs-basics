aca5d33a9664e91f69a80163d264cc1f80a0017d
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-19 13:58:43 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __filePath = "/files/fresh.txt";
+
 const create = async () => {
-    // Write your code here 
+  const fullPath = path.join(__dirname, __filePath);
+  const fileContent = "I am fresh and young";
+  const exists = fs.existsSync(fullPath);
+  if (exists) {
+    throw Error("FS operation failed");
+  } else {
+    fs.writeFile(fullPath, fileContent, "utf8", (err) => {
+      if (err) throw err;
+      console.log("File created");
+    });
+  }
 };
 
 await create();
