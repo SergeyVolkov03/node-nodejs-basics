96f1a47257baf5449df65498360e475c56814213
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-20 12:31:22 +0300

    feat: implement write stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const stream = fs.createWriteStream(filePath, "utf8");
+  process.stdin.pipe(stream);
 };
 
 await write();
