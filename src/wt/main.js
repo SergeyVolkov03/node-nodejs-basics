7cb80a60fb63a262458d9e08bff0cb01d87457a6
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-24 19:35:50 +0300

    feat: implement perform calculations function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import url from "url";
+import path from "path";
+import { cpus } from "os";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "worker");
+const cpuCount = cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const arrayWorkersResult = [];
+  for (let i = 0; i < cpuCount; i++) {
+    arrayWorkersResult.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(filePath, {
+          workerData: {
+            number: 10 + i,
+          },
+        });
+        worker.on("message", resolve);
+        worker.on("error", reject);
+      })
+    );
+  }
+
+  const resultPromises = await Promise.allSettled(arrayWorkersResult);
+  const result = resultPromises.map((res) => {
+    if (res.status === "fulfilled") {
+      return { status: "resolved", value: res.value };
+    } else {
+      return { status: "error", value: null };
+    }
+  });
+  console.log(result);
 };
 
 await performCalculations();
