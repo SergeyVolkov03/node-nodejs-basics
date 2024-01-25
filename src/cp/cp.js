0364bb44dadf4f7e63b5e4330f19f313ae1b2a2f
Sergey Volkov <serzhvolkov03@gmail.com>
2024-01-25 14:50:54 +0300

    feat: implement function spawn child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import cp from "child_process";
+import url from "url";
+import path from "path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptPath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  if (!args) {
+    args = [];
+  }
+  const child = cp.spawn("node", [scriptPath, ...args]);
+  process.stdin.on("data", (data) => {
+    child.stdin.write(data);
+  });
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
